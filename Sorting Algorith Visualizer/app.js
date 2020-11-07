52461db0e08dcb1d2e60fc2acc10bd0884637500
roi65 <62707934+roi65@users.noreply.github.com>
2020-11-07 21:01:13 +0200

    commit

--- Sorting Algorith Visualizer/app.js
+++ Sorting Algorith Visualizer/app.js
@@ -9,8 +9,10 @@ const mergeSortBtn = document.querySelector('.nav_item_merge_sort');
 const quickSortBtn = document.querySelector('.nav_item_quick_sort');
 let algorithem;
 let interval;
-let timeOut;
-let bubbleSortInterval;
+let j1 = [];
+let j2 = [];
+
+
 
 
 bubbleSortBtn.addEventListener('click', () => {
@@ -53,6 +55,7 @@ function setAlgorithem(algo) {
 }
 
 function sort() {
+    
     switch (algorithem) {
         case 'bubble-sort':
             bubbleSort(arrayCellsList);
@@ -60,9 +63,11 @@ function sort() {
         case 'merge-sort':
             beforeMergeSort(arrayCellsList);
             break;
+        case 'heap-sort':
+            beforeHeapSort(arrayCellsList);
+            break;
     }
 }
-
 
 
 function createElement() {
@@ -157,7 +162,7 @@ function bubbleSort(array) {
 
     }
     let i = 0;
-    bubbleSortInterval = setInterval(() => {
+    let bubbleSortInterval = setInterval(() => {
         if (i === j1.length) {
             clearInterval(bubbleSortInterval);
             return;
@@ -168,65 +173,108 @@ function bubbleSort(array) {
 }
 
 
+function beforeHeapSort(array){
+    let numbers = [];
+    numbers = Array.from(array.childNodes);
+    for(let i = 0; i < numbers.length; i++){
+        numbers[i] = numbers[i].value;
+    }
+    heapSort(numbers);
 
+}
+
+
+function heapSort(array){
+
+    let n = array.length;
+    for(let i = Math.floor(n / 2)  - 1; i >=0; i--)
+        heapify(array,n,i);
+
+    for(let i = n-1; i > 0; i--){
+        j1.push(0);
+        j2.push(i);
+        let temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        heapify(array,i,0);
+    }
+
+    let i = 0;
+    let heapSortInteval = setInterval(() => {
+        if (i === j1.length) {
+            clearInterval(heapSortInteval);
+            return;
+        }
+        switchNodes(j1[i], j2[i], arrayCellsList.children);
+        i++;
+    }, 100);
+
+}
+
+function heapify(array, n, i){
+
+    let largest = i;
+    let l = 2 * i + 1;
+    let r = 2 * i  + 2;
+
+    if(l < n && array[l] > array[largest])
+        largest = l;
+    if(r < n && array[r] > array[largest])
+        largest = r;
+
+    if(largest != i){
+        j1.push(i);
+        j2.push(largest);
+        let swap = array[i];
+        array[i] = array[largest];
+        array[largest] = swap;
+        heapify(array,n,largest);
+    }
+}
 
 
 function beforeMergeSort(array) {
     let numbers = [];
     numbers = Array.from(array.childNodes);
     numbers = mergeSort(numbers);
-   
 }
-
-
-function switchNodes(i, j, items) {
-    items[i].style.background = 'black';
-    items[j].style.background = 'rgba(20,202,10)';
-    setTimeout(() => {
-        items[i].style.background = 'blue';
-        items[j].style.background = 'blue';
-    }, 35)
-    temp = items[i].cloneNode(true);
-    items[i].parentNode.replaceChild(items[j].cloneNode(true), items[i]);
-    items[j].parentNode.replaceChild(temp, items[j]);
-}
-
 
 function mergeSort(array) {
-   
+
     let items = arrayCellsList.children;
-    let leftIndex,rightIndex;
+    let leftIndex, rightIndex;
     if (array.length <= 1)
         return array;
 
-        let middle = Math.ceil(array.length / 2);
-        //slice the array into two.
-        const left = mergeSort(array.splice(0, middle));
-        const right = mergeSort(array.splice(-middle));
+    let middle = Math.ceil(array.length / 2);
+    //slice the array into two.
+    const left = mergeSort(array.splice(0, middle));
+    const right = mergeSort(array.splice(-middle));
 
-        for(let i = 0; i < items.length; i++){
-            if(left[0].id === items[i].id)
-            leftIndex=i;
-        }
-        for(let i = 0; i < items.length; i++){
-            if(right[0].id === items[i].id)
+    for (let i = 0; i < items.length; i++) {
+        if (left[0].id === items[i].id)
+            leftIndex = i;
+    }
+    for (let i = 0; i < items.length; i++) {
+        if (right[0].id === items[i].id)
             rightIndex = i;
-        }
+    }
 
-        
+
     return merge(left, right);
 }
 
 function merge(left, right) {
     let resultArray = [];
     let i = j = 0;
-    let rangeAraay = [];
+    let rangeArray = [];
     while (i < left.length && j < right.length) {
         if (left[i].value < right[j].value) {
-                        resultArray.push(left[i++]);
+            animateArray.push(left[i], right[j]);
+            resultArray.push(left[i++]);
         }
         else {
-     
+            animateArray.push(right[j], left[i]);
             resultArray.push(right[j++]);
         }
     }
@@ -237,58 +285,88 @@ function merge(left, right) {
         resultArray.push(right[j++]);
     }
 
-    for(let i = 0; i < resultArray.length; i++){
-        rangeAraay.push(resultArray[i].id);
+    for (let i = 0; i < resultArray.length; i++) {
+        rangeArray.push(resultArray[i].id);
     }
-   
-    replaceNodes(rangeAraay);
+    replaceNodes(rangeArray);
     return resultArray;
 }
 
-function replaceNodes(range){
+
+function replaceNodes(range) {
 
     let items = arrayCellsList.children;
     let rangeArray = [];
     let resultArray = [];
-    let min,max;
-    range.forEach(item=> rangeArray.push(Number.parseInt(item)));
-    min = rangeArray.reduce((curr,prev)=>curr < prev ? curr : prev);
-    max = rangeArray.reduce((curr,prev)=>curr > prev ? curr : prev);
+    let min, max;
+    range.forEach(item => rangeArray.push(Number.parseInt(item)));
+    min = rangeArray.reduce((curr, prev) => curr < prev ? curr : prev);
+    max = rangeArray.reduce((curr, prev) => curr > prev ? curr : prev);
 
-    rangeArray.map(item=>{
-        for(let i = 0; i < items.length; i++){
-            if(item == (items[i].id))
-            resultArray.push(items[i]);
+    rangeArray.map(item => {
+        for (let i = 0; i < items.length; i++) {
+            if (item == (items[i].id))
+                resultArray.push(items[i]);
         }
     });
 
+
+
     let j = 0;
     let i = min;
-        let interval = setInterval(()=>{
-            if(i === max)
+    let interval = setInterval(() => {
+        if (i === max)
             clearInterval(interval);
-            
-            let index;
-            let temp = items[i].cloneNode(true);
-    
-            for(let k = 0; k < items.length; k++){
-                if(items[k].id == resultArray[j].id){
-                    index = k;
-                }
+
+        let index;
+        let temp = items[i].cloneNode(true);
+
+        for (let k = 0; k < items.length; k++) {
+            if (items[k].id == resultArray[j].id) {
+                index = k;
             }
-            items[i].parentElement.replaceChild(resultArray[j].cloneNode(true), items[i]);
-            items[i].parentElement.replaceChild(temp, items[index]);
-            j++;
-            i++;
-        } ,30)
-     
+        }
+
+        let temp2 = resultArray[j].cloneNode(true);
+        temp.style.backgroundColor = 'rgb(20,202,10)';
+        temp2.style.backgroundColor = 'rgb(20,202,10)';
+        setTimeout(() => {
+            temp2.style.backgroundColor = 'darkblue';
+            temp.style.backgroundColor = 'darkblue';
+        }, 20);
+
+        items[i].parentElement.replaceChild(temp2, items[i]);
+        items[i].parentElement.replaceChild(temp, items[index]);
+
+        j++;
+        i++;
+    }, 50)
+
 }
 
 
+function switchNodes(i, j, items) {
+
+    if(algorithem === 'heap-sort')
+        items[i].style.background = 'rgb(20,202,10)';
+    else
+        items[i].style.background = 'black';
+        items[j].style.background = 'rgb(20,202,10)';
+        setTimeout(() => {
+            items[i].style.background = 'blue';
+            items[j].style.background = 'blue';
+        }, 35)
+        temp = items[i].cloneNode(true);
+        items[i].parentNode.replaceChild(items[j].cloneNode(true), items[i]);
+        items[j].parentNode.replaceChild(temp, items[j]);
+
+}
+
+
+
 function clear() {
-    if (interval || timeOut) {
+    if (interval) {
         clearInterval(interval);
-        clearTimeout(timeOut);
     }
 
 }
